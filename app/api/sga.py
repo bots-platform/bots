e09f7c93455a5005f32580c38bc7dc94a9c6da7b
mozadev <ceosmore@gmail.com>
2025-01-27 14:47:43 -0500

    add service_CMR_client automate extraction of data

--- app/api/sga.py
+++ app/api/sga.py
@@ -1,19 +1,23 @@
 from pydantic import ValidationError
 from app.modules.sga.models import FechaSecuenciaRequest
 from fastapi import APIRouter, HTTPException
-from ..modules.sga.service import SGAService
+from ..modules.sga.service_tecnico_operaciones import SGAService
+from ..modules.sga.service_CRM_clientes import send_alert
 
 router = APIRouter(prefix="/api/sga", tags=["sga"])
 
-# @router.post("/reporte")
-# async def generate_dynamic_report(request: FechaSecuenciaRequest):
-#     sga_service = SGAService()
-#     return await sga_service.generate_dynamic_report(request.fecha_inicio, request.fecha_fin)
+@router.post("/reporte")
+async def generate_dynamic_report(request: FechaSecuenciaRequest):
+    sga_service = SGAService()
+    return await sga_service.generate_dynamic_report(request.fecha_inicio, request.fecha_fin)
       
-        
-# @router.get("/reporte")
-# async def generate():
-#     return {'hola': 'sga - tickets'}
+@router.post("/reporteEnvioAlerta")
+def generate_send_alert():
+    return send_alert()
+
+@router.get("/reporte")
+async def generate():
+    return {'hola': 'sga - tickets'}
 
 
 
