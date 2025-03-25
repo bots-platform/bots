038625565edf1f4013a975742efa434476be48a4
cesarmozareyes1 <cesarmozareyes@gmail.com>
2025-03-25 15:01:48 -0500

    Add determine_tipo_reporte function on sga report 335

---
+++ app/modules/sga/minpub/report_validator/service/transform_sga_335/transform_sga_335.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from pathlib import Path
+pd.set_option('display.max_rows', None)
+import re
+
+BASE_DIR = Path.cwd().parent.parent.parent.parent.parent.parent.parent
+SAVE_DIR_EXTRACT_SGA_335 = BASE_DIR / "media" / "minpub" / "validator_report" / "extract" / "sga_335"/"minpuSGA.xlsx"  
+
+df = pd.read_excel(SAVE_DIR_EXTRACT_SGA_335)
+df.head(3)
+
+def determine_tipo_reporte(row):
+    
+    if row['nro_incidencia'] in ["21713978", "21706774"]:
+        return "PROACTIVO"
+    if row['tipo_caso'] == "OTROS CALIDAD-MONITOREO" and row['tipo_servicio'] == "Acceso Dedicado a Internet":
+        return "PROACTIVO"
+    if row['tipo_caso'] == "ENLACE INTERMITENTE - MONITOREO" and row['tipo_servicio'] == "Red Privada Virtual Local":
+        return "PROACTIVO"
+    #if row['Conteo para proactivo'] == 1:
+        return "PROACTIVO"
+    return "RECLAMO"
+
+df.fillna({"nro_incidencia": "", "tipo_caso": "", "tipo_servicio": "", "Conteo para proactivo": 0}, inplace= True)
+df["tipo_reporte"] = df.apply(determine_tipo_reporte, axis=1)
