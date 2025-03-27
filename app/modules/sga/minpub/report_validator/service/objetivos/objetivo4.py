4894e45a5535796e6991cbefe61a4f2b57aba6b7
mozadev <ceosmore@gmail.com>
2025-03-27 14:05:46 -0500

    feat: Add functions scaffolding and parameters

---
+++ app/modules/sga/minpub/report_validator/service/objetivos/objetivo4.py
@@ -0,0 +1,9 @@
+
+
+def validate_coherencia_responsable_determinacioncausa_componente(corte_excel):
+    raise NotImplementedError
+
+
+def objetivo_4(corte_excel):
+
+    validate_coherencia_responsable_determinacioncausa_componente(corte_excel)
