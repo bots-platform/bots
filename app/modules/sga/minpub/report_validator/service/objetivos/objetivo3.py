f90210d0392393c17bd956e0a33686c2e19c0de7
mozadev <ceosmore@gmail.com>
2025-03-27 14:05:10 -0500

    feat: Add functions scaffolding and parameters

---
+++ app/modules/sga/minpub/report_validator/service/objetivos/objetivo3.py
@@ -0,0 +1,16 @@
+
+
+def validate_anexos_sustentos_paradas_reloj(datos_word, telefonia_word, sga_dinamico_380_parada, corte_excel):
+    raise NotImplementedError
+
+def validate_anexos_sustentos_fallas_terceros(datos_word, telefonia_word, corte_excel):
+    raise NotImplementedError
+
+
+def objetivo_3(datos_word, telefonia_word, sga_dinamico_380_parada, corte_excel):
+
+    validate_anexos_sustentos_paradas_reloj(datos_word, telefonia_word, sga_dinamico_380_parada, corte_excel)
+    validate_anexos_sustentos_fallas_terceros(datos_word, telefonia_word, corte_excel)
+
+
+
