55ae1ea52aac3e0c39b874cd57df557a9ea70a3e
cesarmozareyes1 <cesarmozareyes@gmail.com>
2025-03-27 12:45:46 -0500

    feat: Add functions scaffolding and parameters

---
+++ app/modules/sga/minpub/report_validator/service/objetivos/objetivo2.py
@@ -0,0 +1,15 @@
+
+
+def validate_cuadroa_verias_word(corte_excel, datos_word, telefonia_word):
+    raise NotImplementedError
+
+
+def validate_informes_tecnicos(corte_excel, datos_word, telefonia_word):
+    raise NotImplementedError
+
+
+def objetivo_2(corte_excel, datos_word, telefonia_word):
+    
+    validate_cuadroa_verias_word(corte_excel, datos_word, telefonia_word)
+    validate_informes_tecnicos(corte_excel, datos_word, telefonia_word)
+
