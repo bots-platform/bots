41e65cf2735259058f1e71d178a654f65149a856
cesarmozareyes1 <cesarmozareyes@gmail.com>
2025-03-27 14:06:20 -0500

    feat: Add functions scaffolding and parameters

---
+++ app/modules/sga/minpub/report_validator/service/objetivos/objetivo5.py
@@ -0,0 +1,10 @@
+
+
+def validate_cumplimiento_sla_fin_inicio_HHMM(corte_excel):
+    raise NotImplementedError
+
+
+def objetivo_5(corte_excel):
+
+    validate_cumplimiento_sla_fin_inicio_HHMM(corte_excel)
+    
