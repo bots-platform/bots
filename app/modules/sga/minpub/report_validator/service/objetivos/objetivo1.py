4202079465855980d82fc99e3ae86c482396a1b8
mozadev <ceosmore@gmail.com>
2025-03-27 12:37:28 -0500

    feat: Add functions scaffolding and parameters

---
+++ app/modules/sga/minpub/report_validator/service/objetivos/objetivo1.py
@@ -0,0 +1,70 @@
+
+
+def validation_cuismmp_df(corte_excel, sga_dinamico_335, sharepoint_cid_cuismp ):
+    raise NotImplementedError
+
+
+def validation_fecha_hora_incio_fecha_hora_fin(corte_excel, sga_dinamico_335):
+   raise NotImplementedError
+
+
+def validation_fin_inicio_HHMM(corte_excel, sga_dinamico_335):
+    raise NotImplementedError
+
+
+def validation_tiempo_HHMM(sga_dinamico_335, sga_dinamico_380_parada):
+    raise NotImplementedError
+
+
+def validation_tipo_caso_cid_masivo_codincidencia_padre_determinacion_causa(corte_excel, sga_dinamico_335):
+    raise NotImplementedError
+
+
+def validation_averia_tipificacion_problema(corte_excel, sga_dinamico_335):
+    raise NotImplementedError
+
+
+def validation_tipo_reporte_observacion(corte_excel, sga_dinamico_335, sharepoint_cid_cuismp):
+    raise NotImplementedError
+
+
+def validation_medidas_correctivas(corte_excel):
+    raise NotImplementedError
+
+
+def validation_responsable(sga_dinamico_335):
+    raise NotImplementedError
+
+
+def validation_duracion_entero_agrupacion_entero(corte_excel):
+    raise NotImplementedError
+
+
+def validation_indisponibilidad(corte_excel, sga_dinamico_380_parada ):
+    raise NotImplementedError
+
+
+
+def objetivo_1(corte_excel, sga_dinamico_335, sga_dinamico_380_parada,sharepoint_cid_cuismp ):
+
+    validation_cuismmp_df(corte_excel, sga_dinamico_335, sharepoint_cid_cuismp)
+    validation_fecha_hora_incio_fecha_hora_fin(corte_excel, sga_dinamico_335)
+    validation_fin_inicio_HHMM(corte_excel, sga_dinamico_335)
+    validation_tiempo_HHMM(sga_dinamico_335, sga_dinamico_380_parada)
+    validation_tipo_caso_cid_masivo_codincidencia_padre_determinacion_causa(corte_excel, sga_dinamico_335)
+    validation_averia_tipificacion_problema(corte_excel, sga_dinamico_335)
+    validation_tipo_reporte_observacion(corte_excel, sga_dinamico_335, sharepoint_cid_cuismp)
+    validation_medidas_correctivas(corte_excel)
+    validation_responsable(sga_dinamico_335)
+    validation_duracion_entero_agrupacion_entero(corte_excel)
+    validation_indisponibilidad(corte_excel, sga_dinamico_380_parada )
+
+
+
+
+
+
+
+
+
+
