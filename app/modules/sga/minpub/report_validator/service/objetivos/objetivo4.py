e42e9683019f4515436ccb99537159eef24ba2b3
cesarmozareyes1 <cesarmozareyes@gmail.com>
2025-03-27 14:06:49 -0500

    feat: Add functions scaffolding and parameters

--- app/modules/sga/minpub/report_validator/service/objetivos/objetivo4.py
+++ app/modules/sga/minpub/report_validator/service/objetivos/objetivo4.py
@@ -1,9 +1,9 @@
 
 
-def validate_coherencia_responsable_determinacioncausa_componente(corte_excel):
+def validate_coherencia_responsable_determinacion_causa_componente(corte_excel):
     raise NotImplementedError
 
 
 def objetivo_4(corte_excel):
 
-    validate_coherencia_responsable_determinacioncausa_componente(corte_excel)
+    validate_coherencia_responsable_determinacion_causa_componente(corte_excel)
