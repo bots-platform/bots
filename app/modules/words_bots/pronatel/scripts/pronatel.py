ac8c85f54dbef0a37c3ec352f3f573f44e1a013c
cesarmozareyes1 <cesarmozareyes@gmail.com>
2025-04-19 19:21:04 -0500

    git add create reporte pronatel by ticket from excel to wor

---
+++ app/modules/words_bots/pronatel/scripts/pronatel.py
@@ -0,0 +1,198 @@
+from docxtpl import DocxTemplate
+from pathlib import Path
+import numpy as np
+import re
+import pandas as pd
+
+import datetime
+import os
+from typing import List
+import pandas as pd
+from docxtpl import DocxTemplate
+from datetime import datetime
+
+    
+def apply_clean_format(reportes):
+
+    # Se utiliza una list comprehension para generar una nueva lista de reportes.
+    # Si en un reporte existe 'it_medidas_tomadas' y es una cadena, se actualiza esa clave.
+    return [
+            {**reporte,
+              "it_medidas_tomadas": remove_last_lines(
+                reporte["it_medidas_tomadas"]
+                )
+            }
+            if "it_medidas_tomadas" in reporte and isinstance(reporte["it_medidas_tomadas"],str) 
+            else reporte
+            for reporte in reportes
+        ]
+
+
+
+def remove_last_lines( text):
+    DATE_PATTERN = re.compile(
+        r"(?i)^\s*fecha\s*y\s*hora\s*(de\s*)?(inicio|fin)?:?\s*\d{1,2}/\d{1,2}/\d{4}\s*\d{1,2}:\d{2}\s*$"
+    )
+    lines = text.split("\n")
+    while lines and DATE_PATTERN.search(lines[-1].strip()):
+        lines.pop()
+    return "\n".join(lines)
+
+
+def validate_required_columns_from_excel(excel_file):
+    required_columns = [
+        'nro_incidencia',  # ticket
+        'canal_ingreso',  # tipo generacion ticket
+        'interrupcion_inicio',  # fecha/hora interrupcion
+        'fecha_generacion',  # fecha/hora generacion
+        'interrupcion_fin',  # fecha/ hora subsanacion
+        'cid',  # CID
+        'tipo_caso',  # Tipo Caso
+        'tipificacion_problema',  # averia
+        'it_determinacion_de_la_causa',  # DETERMINACION DE LA CAUSA
+        'it_medidas_tomadas',  # MEDIDAS CORRECTIVAS Y/O PREVENTIVAS TOMADAS
+        'it_conclusiones',  # RECOMENDACIONES
+        'tiempo_interrupcion',  # tiempo subsanacion efectivo
+        'tipificacion_interrupcion',  # tiempo de indisponibilidad
+        'tipificacion_tipo',  # ATRIBUIBLE
+        'fecha_comunicacion_cliente'  # Fecha hora solicitud  
+    ]
+    try:
+        df = pd.read_excel(excel_file, dtype=str, engine="openpyxl")
+    except Exception as e:
+        raise ValueError(f"Error 400: Could not read Excel file: {str(e)}")
+    
+    df.columns = df.columns.str.strip()
+
+    missing_columns = [col for col in required_columns if col not in df.columns]
+    if missing_columns:
+        error_message = [
+            f"Error 400: Columnas requeridas faltantes en el archivo Excel. "
+            f"Columnas faltantes: {missing_columns}",
+            "Por favor, compruebe que los nombres de las columnas de su archivo Excel coinciden exactamente."
+        ]
+        raise ValueError(error_message)
+    
+    df_clean = df[required_columns].apply(
+        lambda col: col.str.replace("_x000D_", "", regex=False).str.strip()
+    ).fillna("-")
+
+    return df_clean
+
+
+def fill_column_mejoras_and_recomendaciones(df):
+    """
+    Split 'it_conclusiones' into 'mejoras' and 'recomendaciones'.
+    - 'recomendaciones' will contain the text starting from the line that contains 'recomienda'.
+    - 'mejoras' will contain the text above that line.
+    """
+    if 'mejoras' not in df.columns:
+        df['mejoras'] = ''
+    if 'recomendaciones' not in df.columns:
+        df['recomendaciones'] = ''
+    
+    def process_text(text):
+        lines = text.split('\n')
+        split_index = None
+
+        for i, line in enumerate(lines):
+            if 'recomienda' in line.lower():
+                split_index = i
+                break
+        
+        if split_index is not None:
+            return '\n'.join(lines[:split_index]), '\n'.join(lines[split_index:])
+        return text, '' 
+    
+    df[['mejoras', 'recomendaciones']] = df['it_conclusiones'].apply(
+        lambda text: pd.Series(process_text(text))
+    )
+    
+    return df
+
+
+def create_reportes_by_ticket_by_client(df, word_template_path, word_output_path):
+
+
+    df['canal_ingreso'] = df['canal_ingreso'].replace({
+        'e-mail': 'Reportado por el usuario',
+        'Telefono': 'Reportado por el usuario',
+        'Interno': 'Reportado por el usuario'
+       })
+    
+
+    df['fecha_hora_llegada_personal'] = "-"
+    df['tiempo_llegada_personal'] = "-"
+    df['horas_excedidas_plazo_reparacion_bases'] = 0
+    df['descripcion_problema'] = 'Se detectó la pérdida de gestión del servicio en los Sistemas de Monitoreo de Claro.'
+    
+    df['interrupcion_inicio'] = pd.to_datetime(df['interrupcion_inicio'], errors='coerce')
+    df['fecha_generacion'] = pd.to_datetime(df['fecha_generacion'], errors='coerce')
+    df['interrupcion_fin'] = pd.to_datetime(df['interrupcion_fin'], errors='coerce')
+    df['fecha_comunicacion_cliente'] = pd.to_datetime(df['fecha_comunicacion_cliente'], errors='coerce')
+
+   
+
+    df_sorted = df.sort_values(by='interrupcion_inicio', ascending=True)
+
+    df_sorted['interrupcion_inicio'] = df_sorted['interrupcion_inicio'].dt.strftime('%d/%m/%Y %H:%M')
+    df_sorted['fecha_generacion'] = df_sorted['fecha_generacion'].dt.strftime('%d/%m/%Y %H:%M')
+    df_sorted['interrupcion_fin'] = df_sorted['interrupcion_fin'].dt.strftime('%d/%m/%Y %H:%M')
+
+    df_sorted['fecha_comunicacion_cliente'] = df_sorted['fecha_comunicacion_cliente'].dt.strftime('%d/%m/%Y %H:%M')
+
+    df_sorted['fecha_comunicacion_cliente'] = np.where(
+        df_sorted['canal_ingreso'] == 'Proactivo',
+        '-',
+        df_sorted['fecha_comunicacion_cliente']
+        ) 
+
+
+    df_sorted['tipificacion_problema'] = df_sorted['tipificacion_problema'].str.split('-').str[0].str.strip()
+    
+    df_sorted["tipificacion_problema"] = df_sorted["tipificacion_problema"].apply(
+    lambda x: "PROBLEMA DE ENERGIA COMERCIAL EN SITE/POP" if "PROBLEMA DE ENERGIA COMERCIAL EN SITE/POP" in x else x
+)   
+    
+    df_sorted['tipificacion_tipo'] = df_sorted['tipificacion_tipo'].replace({
+        'CLARO - DEGRADACION': 'CLARO', 
+        'TERCEROS - CORTE' : 'TERCEROS'
+       })
+   
+    
+    BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent.parent.parent
+    file_path_sedes = BASE_DIR / "media" / "pronatel" / "sedes" / "RELACIÓN DE CADs CON CID.xlsx"
+
+    if not  file_path_sedes.exists():
+            raise FileNotFoundError(f" File not found {file_path_sedes}")
+    
+    df_sedes_by_cid = pd.read_excel(file_path_sedes)
+
+    df_sorted["cid"] = df_sorted["cid"].astype(str)  
+    df_sedes_by_cid["cid"] = df_sedes_by_cid["cid"].astype(str)
+    
+    df_sorted = df_sorted.merge(df_sedes_by_cid, on="cid", how="left")
+
+    df_sorted = fill_column_mejoras_and_recomendaciones(df_sorted)
+    
+    reportes_detallados_dict = df_sorted.to_dict(orient="records")
+
+    reporte_detallado_clean_dict = apply_clean_format(reportes_detallados_dict)
+
+    doc = DocxTemplate(word_template_path)
+    
+    context  = {
+        'titulo': f" 01/04/2025 hasta 01/01/2025 ",
+        'cliente': "PRONATEL",
+        'reportes': reporte_detallado_clean_dict
+        }
+    output_file = os.path.join(word_output_path, f"reporte_{datetime.now().strftime('%Y%m%d_%H%M%S')}.docx")
+    doc.render(context)
+    doc.save(output_file)
+    print(f"Reporte mensual generado exitosamente: {output_file}")
+    return output_file
+
+
+
+
+
