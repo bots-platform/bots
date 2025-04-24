0c6e9ad5230973567472bde2ad06b48564bf0c31
mozadev <ceosmore@gmail.com>
2025-04-24 17:32:18 -0500

    add column responsable on df return by table_index

--- app/modules/sga/minpub/report_validator/service/objetivos/objetivo_2/word_averias_extractor.py
+++ app/modules/sga/minpub/report_validator/service/objetivos/objetivo_2/word_averias_extractor.py
@@ -17,27 +17,49 @@ def log_exceptions(func):
     return wrapper
 
 @log_exceptions
-def extract_averias_table(path_docx: str, table_index: int = 0) -> pd.DataFrame:
+def extract_averias_table(path_docx: str) -> pd.DataFrame:
     """
     Load the specified table from a .docx and return it as a DataFrame.
+    index = 4 (CLARO)
+    index = 5 (CLIENTE)
+    index =  (TERCEROS)
     We assume the first row is the header.
     """
+
+    table_index_list = [4,5,6]
+
     doc = Document(path_docx)
-    if table_index >= len(doc.tables):
-        raise IndexError(f"Document only has {len(doc.tables)} tables.")
+    all_dfs: List[pd.DataFrame] = []
+   
 
-    table = doc.tables[table_index]
+    for table_index in table_index_list:
 
-    headers = [cell.text.strip() for cell in table.rows[0].cells]
+        if table_index >= len(doc.tables):
+            raise IndexError(f"Document only has {len(doc.tables)} tables.")
+        
+        table = doc.tables[table_index]
 
-    records: List[Dict] = []
-    for row in table.rows[1:]:
-        vals = [cell.text.strip() for cell in row.cells]
-     
-        if len(vals) < len(headers):
-            vals += [""] * (len(headers) - len(vals))
-        records.append(dict(zip(headers, vals)))
+        headers = [cell.text.strip() for cell in table.rows[0].cells]
 
-    df = pd.DataFrame.from_records(records)
-    return df
+        records: List[Dict] = []
+        for row in table.rows[1:]:
+            vals = [cell.text.strip() for cell in row.cells]
 
+            if len(vals) < len(headers):
+                vals += [""] * (len(headers) - len(vals))
+            records.append(dict(zip(headers, vals)))
+
+        df_part = pd.DataFrame.from_records(records)
+        
+        if table_index == 4:
+            df_part['responsable'] = 'CLARO'
+        elif table_index == 5:
+            df_part['responsable'] = 'CLIENTE'
+        else: 
+            df_part['responsable'] = 'TERCEROS'
+
+        all_dfs.append(df_part)
+
+    result = pd.concat(all_dfs, ignore_index=True)
+
+    return result
