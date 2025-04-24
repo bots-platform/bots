035e9e0651ff9f317f6a58bff5cadf70e684b263
mozac <cesarmozareyes@gmail.com>
2025-04-24 15:22:03 -0500

    add validation_objetivo_3 who call all functions about this validations3

--- app/modules/sga/minpub/report_validator/service/objetivos/objetivo_3/objetivo_3.py
+++ app/modules/sga/minpub/report_validator/service/objetivos/objetivo_3/objetivo_3.py
@@ -1,3 +1,58 @@
-from app.modules.sga.minpub.report_validator.service.objetivos.objetivo_1.o11_indisponibilidad_validator import (
-    validate_indisponibilidad
+import pandas as pd
+
+
+from app.modules.sga.minpub.report_validator.service.objetivos.objetivo_3.o1_anexos_sustentos_paradas_reloj_validator    import (
+    validate_anexos_indisponibilidad_word, build_failure_messages_validate_anexos_indisponibilidad_word
 )
+
+
+from utils.logger_config import get_sga_logger
+logger = get_sga_logger()
+
+def log_exceptions(func):
+    """
+    Decorator to log exceptions in a function using the shared 'logger'.
+    It will also re-raise the exception so that the caller can handle it
+    appropriately (e.g., fail fast or continue).
+    """
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as exc:
+            logger.error(
+                f"Error in function '{func.__name__}': {exc}",
+                exc_info=True
+            )
+            # Optionally, decide whether to re-raise or swallow the exception.
+            # Usually best practice is to re-raise so the pipeline can decide what to do:
+            raise
+    return wrapper
+
+
+@log_exceptions
+def validation_objetivo_3(
+df_matched_word_datos_anexo_indisponibilidad_corte_excel,
+df_matched_word_telefonia_anexo_indisponibilidad_corte_excel
+   
+
+) -> pd.DataFrame:
+    """
+    Aggregates all sub-validations for Objective 2.
+    Uses a common merge function and passes the merged data to each sub-validation.
+    
+    Returns a DataFrame with the failure details for Objective 1.
+    """
+    df_validate_anexos_indisponibilidad_word_datos = validate_anexos_indisponibilidad_word(df_matched_word_datos_anexo_indisponibilidad_corte_excel, componente_word = 'COMPONENTE II')
+    df_failures_message_validate_anexos_indisponibilidad_word_datos = build_failure_messages_validate_anexos_indisponibilidad_word(df_validate_anexos_indisponibilidad_word_datos)
+
+    df_validate_anexos_indisponibilidad_word_telefonia = validate_anexos_indisponibilidad_word(df_matched_word_telefonia_anexo_indisponibilidad_corte_excel, componente_word = 'COMPONENTE IV')
+    df_failures_message_validate_anexos_indisponibilidad_word_telefonia = build_failure_messages_validate_anexos_indisponibilidad_word(df_validate_anexos_indisponibilidad_word_telefonia)    
+
+    df_failures = pd.concat(
+        [
+        df_failures_message_validate_anexos_indisponibilidad_word_datos,
+        df_failures_message_validate_anexos_indisponibilidad_word_telefonia
+        ],
+        ignore_index=True)
+
+    return df_failures
