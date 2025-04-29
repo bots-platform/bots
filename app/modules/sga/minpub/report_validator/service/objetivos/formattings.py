fc6109284aa13ca246e9847b906ce4083915e5cf
mozadev <ceosmore@gmail.com>
2025-04-28 22:17:12 -0500

    chore: add segundos set 00 on paragraph-clock stops to compare with INDISPONIBLIDAD column from EXCEL

--- app/modules/sga/minpub/report_validator/service/objetivos/formattings.py
+++ app/modules/sga/minpub/report_validator/service/objetivos/formattings.py
@@ -11,8 +11,8 @@ from app.modules.sga.minpub.report_validator.service.objetivos.decorators import
 @log_exceptions
 def _format_interval(dt_start, dt_end) -> str:
     return (
-        f"{dt_start.strftime('%d/%m/%Y %H:%M')} "
-        f"hasta el día {dt_end.strftime('%d/%m/%Y %H:%M')}"
+        f"{dt_start.strftime('%d/%m/%Y %H:%M:%S')} "
+        f"hasta el día {dt_end.strftime('%d/%m/%Y %H:%M:%S')}"
     )
 
 
@@ -29,10 +29,7 @@ def make_paragraph_paradas_cliente(stops_list):
         mm = rem // 60
         total_str = f"{hh:02d}:{mm:02d}"
 
-        header = (
-            "Se tuvo indisponibilidad por parte del cliente "
-            "para continuar los trabajos el/los día(s)"
-        )
+        header = "Se tuvo indisponibilidad por parte del cliente para continuar los trabajos el/los día(s)".strip()
         body = "\n".join(lines)
         footer = f"(Total de horas sin acceso a la sede: {total_str} horas)"
         return f"{header}\n{body}\n{footer}"
