ed4232b81ae01aae4278f5a966df6a60253ec512
mozadev <ceosmore@gmail.com>
2025-05-05 19:01:23 -0500

    chore (cleanup) : remove absolete modules after ETL refactor

--- app/modules/sga/minpub/report_validator/service/objetivos/etl/merge/cuadro_averias/word_telefonia.py
+++ app/modules/sga/minpub/report_validator/service/objetivos/etl/merge/cuadro_averias/word_telefonia.py
@@ -2,13 +2,14 @@ from typing import List, Dict
 import pandas as pd
 from datetime import datetime, timedelta
 
-from app.modules.sga.minpub.report_validator.service.objetivos.decorators import ( 
+from app.modules.sga.minpub.report_validator.service.objetivos.utils.decorators import ( 
     log_exceptions
 )
 
+
 @log_exceptions
-def merge_word_datos_averias_corte_excel(
-        df_word_datos_averias: pd.DataFrame,
+def merge_word_telefonia_averias_corte_excel(
+        df_word_telefonia_averias: pd.DataFrame,
         df_corte_excel: pd.DataFrame,
         match_type:str
     ) -> pd.DataFrame:
@@ -20,16 +21,17 @@ def merge_word_datos_averias_corte_excel(
 
         Returns a merged DataFrame with common columns needed.
         """
-        df_merge_word_datos_corte_excel = pd.merge(
-        df_word_datos_averias,
+        
+        df_merge_word_telefonia_corte_excel = pd.merge(
+        df_word_telefonia_averias,
         df_corte_excel,
         on='nro_incidencia',
         how='left',
         indicator=True,
-        suffixes=('_word_datos_averias', '_corte_excel')
+        suffixes=('_word_telefonia_averias', '_corte_excel')
         )
-    
-        matched_rows = df_merge_word_datos_corte_excel[df_merge_word_datos_corte_excel['_merge'] == match_type]
+           
+        matched_rows = df_merge_word_telefonia_corte_excel[df_merge_word_telefonia_corte_excel['_merge'] == match_type]
         return matched_rows
 
-    
+  
