9e7f87ea9cfc3da978c0bbf8c3cb436781b47ddf
mozadev <ceosmore@gmail.com>
2025-06-02 10:23:46 -0500

    refactor: implement global lock in descarga_reporte function to ensure thread safety during report generation

--- app/api/sga.py
+++ app/api/sga.py
@@ -5,6 +5,8 @@ import os
 from fastapi.responses import FileResponse
 from ..modules.sga.service_tecnico_operaciones import SGAService
 from ..modules.sga.service_crm_clientes import send_alert
+from app.shared.lock import global_lock  # usa el lock global
+
 
 router = APIRouter(prefix="/api/sga", tags=["sga"])
 
@@ -15,7 +17,18 @@ def descarga_reporte(request: FechaSecuenciaRequest):
     indice_tabla_reporte_detalle = 4   # TABLERO 276 AVERIAS Y GESTIONADOS
 
     sga_service = SGAService()
-    ruta_archivo = sga_service.generate_dynamic_report(request.fecha_inicio, request.fecha_fin, indice_tabla_reporte_data_previa, indice_tabla_reporte_detalle)
+    #ruta_archivo = sga_service.generate_dynamic_report(request.fecha_inicio, request.fecha_fin, indice_tabla_reporte_data_previa, indice_tabla_reporte_detalle)
+
+    with global_lock:
+        ruta_archivo = sga_service.generate_dynamic_report(
+            request.fecha_inicio,
+            request.fecha_fin,
+            indice_tabla_reporte_data_previa,
+            indice_tabla_reporte_detalle
+        )
+
+
+
 
     if not os.path.exists(ruta_archivo):
         raise HTTPException(status_code=404, detail="El archivo no fue encontrado")
