be90987b3a4832c89f0bcdb3cf37ed9849464a2e
mozadev <ceosmore@gmail.com>
2025-06-04 18:10:45 -0500

    refactor: enhance failure message clarity in o5_tipo_caso_cid_masivo_cod_padre_determinacion.py by updating descriptions for case type and CID mismatches

--- app/modules/sga/minpub/report_validator/service/objetivos/validators/objetivo_1/o5_tipo_caso_cid_masivo_cod_padre_determinacion.py
+++ app/modules/sga/minpub/report_validator/service/objetivos/validators/objetivo_1/o5_tipo_caso_cid_masivo_cod_padre_determinacion.py
@@ -66,11 +66,11 @@ def build_failure_messages_tipo_caso_cid_masivo_codincidencia_padre_determinacio
                      " \n No coincide Determinacion de la causa Excel-Corte: \n" +
                        df['DETERMINACIÓN DE LA CAUSA'].astype(str)+"\n con SGA Determinación de la causa: \n"+ df['causa_sga335_clean'],  "")+
             np.where(~df['tipo_caso_match'],
-                     "\n No coincide Tipo de Caso de CORTE-EXCEL: \n" + df['TIPO CASO'].astype(str) +"\n con SGA determinación de la causa: \n\n" + 
+                     "\n No coincide Tipo de Caso de CORTE-EXCEL: \n" + df['TIPO CASO'].astype(str) +"\n con SGA columna tipo_caso: \n\n" + 
                      df['tipo_caso'].astype(str), "")+
             np.where(~df['cid_match'],
                      "\n No coincide cid  de CORTE-EXCEL : \n"+ df['CID'].astype(str) +
-                     "\n con SGA: \n" + df['cid'].astype(str), "")+
+                     "\n con cid de SGA: \n" + df['cid'].astype(str), "")+
            np.where(~df['cod_incidencia_padre_match'],
                      "\n  No coincide nro incidencia padre EXCEL-CORTE: \n" + df['CODINCIDENCEPADRE'].astype(str) +
                        "con columna SGA codeincidencepadre: \n" +df['codincidencepadre'].astype(str), "") + 
