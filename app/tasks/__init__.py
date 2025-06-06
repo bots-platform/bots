0f28630df9d0158e2e422c94841ad87a11e0e680
mozadev <ceosmore@gmail.com>
2025-06-06 14:43:26 -0500

    feat: update task exports to include keep_system_active_task for enhanced system activity management

--- app/tasks/__init__.py
+++ app/tasks/__init__.py
@@ -1,3 +1,6 @@
-from .automation_tasks import process_minpub_task, process_sga_report_task
+from .automation_tasks import process_minpub_task, process_sga_report_task, keep_system_active_task
 
-__all__ = ["process_minpub_task", "process_sga_report_task"]
+__all__ = ["process_minpub_task", 
+           "process_sga_report_task",
+           "keep_system_active_task"
+           ]
