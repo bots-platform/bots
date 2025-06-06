42ff54ae3469090c7df2e8c374c8e67d89345ecc
mozadev <ceosmore@gmail.com>
2025-06-06 10:31:54 -0500

    feat: add process_sga_report_task to task exports for enhanced reporting capabilities

--- app/tasks/__init__.py
+++ app/tasks/__init__.py
@@ -1,3 +1,3 @@
-from .automation_tasks import process_minpub_task
+from .automation_tasks import process_minpub_task, process_sga_report_task
 
-__all__ = ["process_minpub_task"]
+__all__ = ["process_minpub_task", "process_sga_report_task"]
