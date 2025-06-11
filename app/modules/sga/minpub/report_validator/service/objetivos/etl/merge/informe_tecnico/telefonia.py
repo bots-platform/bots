64acfbd43c777f669757f4b7b43aa5bb213bf20f
mozadev <ceosmore@gmail.com>
2025-06-11 17:57:21 -0500

    feat: refactor merge_word_telefonia_informe_corte_excel function to utilize PySpark for DataFrame merging on 'nro_incidencia', enhancing performance and scalability for Objective 2 reporting

--- app/modules/sga/minpub/report_validator/service/objetivos/etl/merge/informe_tecnico/telefonia.py
+++ app/modules/sga/minpub/report_validator/service/objetivos/etl/merge/informe_tecnico/telefonia.py
@@ -1,36 +1,39 @@
 from typing import List, Dict
-import pandas as pd
+from pyspark.sql import DataFrame
+from pyspark.sql.functions import col, when, broadcast
 from datetime import datetime, timedelta
 
-from app.modules.sga.minpub.report_validator.service.objetivos.utils.decorators import ( 
+from app.modules.sga.minpub.report_validator.service.objetivos.utils.decorators import (
     log_exceptions
 )
 
-
 @log_exceptions
 def merge_word_telefonia_informe_corte_excel(
-        df_word_informe_tecnico_telefonia: pd.DataFrame,
-        df_corte_excel: pd.DataFrame,
-        match_type:str
-    ) -> pd.DataFrame:
-        """
-        Common merge function for Objective 2.
+        df_word_informe_tecnico_telefonia: DataFrame,
+        df_corte_excel: DataFrame,
+        match_type: str
+    ) -> DataFrame:
+    """
+    Common merge function for Objective 2.
 
-        Merges:
-          - corte-excel  with word_telefonia on 'nro_incidencia'
+    Merges:
+      - corte-excel with word_telefonia on 'nro_incidencia'
 
-        Returns a merged DataFrame with common columns needed.
-        """
-        
-        df_merge_word_telefonia_corte_excel = pd.merge(
-        df_word_informe_tecnico_telefonia,
-        df_corte_excel,
+    Returns a merged DataFrame with common columns needed.
+    """
+    
+    # Optimize join by broadcasting the smaller DataFrame
+    df_merge_word_telefonia_corte_excel = df_word_informe_tecnico_telefonia.join(
+        broadcast(df_corte_excel),  # Broadcast the smaller DataFrame to minimize shuffling
         on='nro_incidencia',
-        how='left',
-        indicator=True,
-        suffixes=('_word_telefonia_informe', '_corte_excel')
-        )
-           
-        matched_rows = df_merge_word_telefonia_corte_excel[df_merge_word_telefonia_corte_excel['_merge'] == match_type]
-        return matched_rows
+        how='left'
+    ).withColumn(
+        '_merge',
+        when(col('nro_incidencia').isNotNull(), 'both').otherwise('left_only')
+    )
+       
+    # Filter based on match_type
+    matched_rows = df_merge_word_telefonia_corte_excel.filter(col('_merge') == match_type)
+    
+    return matched_rows
 
